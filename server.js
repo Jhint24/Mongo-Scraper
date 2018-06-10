fdaaeca9d6363420acb9fd2fc4b6802936f2f3f4
Josh <jhint2424@gmail.com>
2018-06-10 02:01:42 -0700

    package

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 const path = require("path");
 const db = require("./models");
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 // Initialize Express
 const app = express();
