a74d9fd3701fc267b0e3a8fdac46dc1f7e6cdacf
Josh <jhint2424@gmail.com>
2018-06-09 22:52:19 -0700

    scrape

--- models/Article.js
+++ models/Article.js
@@ -6,7 +6,6 @@ var Schema = mongoose.Schema;
 // Using the Schema constructor, create a new UserSchema object
 // This is similar to a Sequelize model
 var ArticleSchema = new Schema({
-
   title: {
     type: String,
     required: true,
@@ -18,11 +17,11 @@ var ArticleSchema = new Schema({
     required: true
   },
 
-  summary:  {
-    type: String,
-    required: true,
-    trim: true
-  },
+  // summary:  {
+  //   type: String,
+  //   required: true,
+  //   trim: true
+  // },
   // `note` is an object that stores a Note id
   // The ref property links the ObjectId to the Note model
   // This allows us to populate the Article with an associated Note
--- routes/routes.js
+++ routes/routes.js
@@ -6,45 +6,58 @@ const db = require("../models");
 // A GET route for scraping the echoJS website
 router.get("/scrape", function(req, res) {
   // First, we grab the body of the html with request
-  axios.get("http://www.nytimes.com/").then(function(response) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
-    var $ = cheerio.load(response.data);
+  axios
+    .get("http://www.nytimes.com/")
+    .then(function(response) {
+      // Then, we load that into cheerio and save it to $ for a shorthand selector
+      var $ = cheerio.load(response.data);
 
-    // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
-      // Save an empty result object
-      var result = {};
+      // Now, we grab every h2 within an article tag, and do the following:
+      $("article h2").each(function(i, element) {
+        // Save an empty result object
+        var result = {};
 
-      // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this)
-        .children("a")
-        .text();
-      result.link = $(this)
-        .children("a")
-        .attr("href");
+        // Add the text and href of every link, and save them as properties of the result object
+        result.title = $(this)
+          .children("a")
+          .text();
+        result.link = $(this)
+          .children("a")
+          .attr("href");
 
-      // Create a new Article using the `result` object built from scraping
-      db.Article.create(result)
-        .then(function(dbArticle) {
-          // View the added result in the console
-          console.log(dbArticle);
-        })
-        .catch(function(err) {
+        // Create a new Article using the `result` object built from scraping
+        db.Article.update(
+          { title: result.title },
+          { $set:  result},
+          { upsert: true }
+        ).catch(function(err) {
           // If an error occurred, send it to the client
-          return res.json(err);
+          console.log(err);
         });
+      });
+    })
+    .then(function(result) {
+      console.log("RESULT:", result);
+      res.render("scrape");
+    })
+    .catch(function(err) {
+      console.log("ERR:", err);
+      res.send(err);
     });
-    console.log($);
-    // If we were able to successfully scrape and save an Article, send a message to the client
-  })
-  .catch(function(err) {
-    // If an error occurred, send it to the client
-    return res.json(err);
-  });
-  res.send("Scrape Complete");
 });
+
 router.get("/", function(req, res) {
-  res.render("home");
+  const obj = {};
+  obj.page = "/";
+
+  db.Article.find()
+    .then(function(articles) {
+      obj.articles = articles;
+      res.render("home", obj);
+    })
+    .catch(function(err) {
+      res.send(err);
+    });
 });
 
 router.get("/saved", function(req, res) {
